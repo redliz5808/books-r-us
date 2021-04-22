5855616337797ab76af14c32ce6ec5a8519e35b2
Beth Franklin <66703062+redliz5808@users.noreply.github.com>
2021-04-22 10:25:40 -0400

    Added NYT Review code

---
+++ src/components/NytReviews/nytReviews.styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Title = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+  padding: 5px;
+`
+
+export const Frame = styled.iframe`
+  border: 1px solid #6c698d;
+`
+
+export const NoReview = styled.div`
+  border: 1px solid #6c698d;
+`
--- src/pages/Book/book.styles.js
+++ src/pages/Book/book.styles.js
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+export const Main = styled.div`
+  display: flex;
+  margin-left: 20px;
+`;
+
 export const Container = styled.div`
   display: inline;
   margin-left: 20px;
@@ -15,9 +20,19 @@ export const Favorite = styled.button`
   border: none;
   background: transparent;
   color: #6c698d;
+  padding: 10px;
 `;
 
 export const BookInfo = styled.div`
   display: inline;
   margin-left: 20px;
-`
+`;
+
+export const NytContainer = styled.div`
+  border-radius: 3px;
+  min-width: 500px;
+  min-height: 500px;
+  max-width: 700px;
+  margin-left: 20px;
+  margin-top: 10px;
+`;
